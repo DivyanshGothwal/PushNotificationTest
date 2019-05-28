484b48d1cf00aa58e7e6b58d4a425f80e9cfe61f
Divyansh Gothwal <shotsdivyansh@gmail.com>
2019-05-28 22:28:16 +0530

    changes port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const publicVapidKey =
   "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
 const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
 
-webpush.setVapidDetails(
+webpush.setVapidDetails( 
   "mailto:test@test.com",
   publicVapidKey,
   privateVapidKey
@@ -37,6 +37,6 @@ app.post("/subscribe", (req, res) => {
     .catch(err => console.error(err));
 });
 
-const port = 5000;
+const port = 5004;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
