75ab1a83c24cc086bf958d25bb2c2deedefa55d2
Divyansh Gothwal <shotsdivyansh@gmail.com>
2019-05-28 22:32:36 +0530

    updated port assignment

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.post("/subscribe", (req, res) => {
     .catch(err => console.error(err));
 });
 
-const port = 5004;
+const port = (process.env.PORT ||5004);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
