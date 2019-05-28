ebc8530188b79e491fe14a63a68ad43894c7f1d3
Divyansh Gothwal <shotsdivyansh@gmail.com>
2019-05-28 22:56:47 +0530

    changed service worker

--- client/worker.js
+++ client/worker.js
@@ -5,7 +5,7 @@ self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: "Notified by Traversy Media!",
+    body: "Notified by Divyansh",
     icon: "http://image.ibb.co/frYOFd/tmlogo.png"
   });
 });
